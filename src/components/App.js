589f6be6fd5e8859a08a2eada9c189fe945ac121
DuMark <dumark.allman@yahoo.com>
2023-12-14 11:31:29 -0500

    My head hurts

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+// Step 1...activate the state
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
+//Step 2...create the dark mode
+const [isDarkMode, setisDarkMode]=
+useState(false);
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+
+  //step 3...event handeling the dark mode
+  const toggleDarkMode = () => {
+    setisDarkMode(!isDarkMode);
+  };
+//Step 5...make it dynamic!!!
+  const appClass = isDarkMode ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        {/*Step 6...add even handler to the button */}
+        <button onClick={toggleDarkMode}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,27 @@
-import React from "react";
+// step 1...use state activation?
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  //step 2..got to create the state of the cart
+  const [inCart, setInCart] = 
+  useState(false);
+
+  //steps 3 and 4...make the button and class name dynamic. Oh almost for got the text of the button
+  const listItemClass = inCart ? "in-cart" : "";
+  const buttonText = inCart ? "Remove From Cart" : "Add To Cart";
+
+  //Step 5...Let's not forget the event handler
+  const toggleCart = () => {
+    setInCart(!inCart);
+  };
+
   return (
-    <li className="">
+    <li className={listItemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      {/*Step 6, 7....let's not forget to add some love to the button and place the listItemClass in the blank*/}
+      <button className="add"
+      onClick={toggleCart}> { buttonText} </button>
     </li>
   );
 }
